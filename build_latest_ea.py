ec241783028d39ccc215ce8abb413a7e8a695e71
e <107287902+dejelloper@users.noreply.github.com>
2022-06-14 20:58:31 -0400

    various improvements

--- build_latest_ea.py
+++ build_latest_ea.py
@@ -8,12 +8,12 @@ import shutil
 import subprocess
 import sys
 import tarfile
-import time
 import urllib.error
 from urllib.request import Request, urlopen
 
 YUZU_SRC_LZMA_REGEX = re.compile(
     "^yuzu-windows-msvc-source-[0-9]*-[0-9a-f]*.tar.xz$")
+LATEST_DOWNLOADED_VER_TXT_PATH = "last_downloaded_ver.txt"
 
 if not os.path.exists("TOKEN"):
     print("TOKEN file not found, see README.md :(")
@@ -39,7 +39,6 @@ except urllib.error.HTTPError as httpError:
 
 jwt = jwtResponse.read().decode("utf-8")
 
-
 print("Downloading latest build info...")
 downloadsListingRequest = Request(
     "https://api.yuzu-emu.org/downloads/earlyaccess/")
@@ -54,92 +53,94 @@ except urllib.error.HTTPError as httpError:
 listingData = json.loads(downloadsListingResponse.read())
 latestVersion = listingData["version"]
 
-
-if not os.path.exists("last_downloaded_ver.txt"):
-    temp = open("last_downloaded_ver.txt", "w")
+if not os.path.exists(LATEST_DOWNLOADED_VER_TXT_PATH):
+    temp = open(LATEST_DOWNLOADED_VER_TXT_PATH, "w")
     temp.write("")
     temp.close()
 
+lastDownloadedVersionFile = open(LATEST_DOWNLOADED_VER_TXT_PATH, "r")
+lastDownloaded = lastDownloadedVersionFile.read()
 
-with open("last_downloaded_ver.txt", "r+") as f:
-    lastDownloaded = f.read()
-    if len(lastDownloaded) > 0 and int(lastDownloaded) >= latestVersion:
-        print(f"Already at latest version ({latestVersion})!")
-    else:
-        print(
-            f"Updating to {latestVersion}{f' from {lastDownloaded}' if len(lastDownloaded) > 0 else ''}...")
-        for releaseEntry in listingData["files"]:
-            if YUZU_SRC_LZMA_REGEX.match(releaseEntry["name"]):
-                print("Downloading LZMA...")
-                latestLZMARequest = Request(releaseEntry["url"])
-                latestLZMARequest.add_header("Authorization", f"Bearer {jwt}")
-                latestLZMARequest.add_header(
-                    "User-Agent", "liftinstall (j-selby)")
-                latestLZMAResponse = None
-                try:
-                    latestLZMAResponse = urlopen(latestLZMARequest)
-                except urllib.error.HTTPError as httpError:
-                    print(f"Error downloading LZMA ({httpError.code}) :(")
-                    sys.exit(1)
+if len(lastDownloaded) > 0 and int(lastDownloaded) >= latestVersion:
+    print(f"Already at latest version ({latestVersion})!")
+else:
+    print(
+        f"Updating to {latestVersion}{f' from {lastDownloaded}' if len(lastDownloaded) > 0 else ''}...")
+    for releaseEntry in listingData["files"]:
+        if YUZU_SRC_LZMA_REGEX.match(releaseEntry["name"]):
+            print("Downloading LZMA...")
+            latestLZMARequest = Request(releaseEntry["url"])
+            latestLZMARequest.add_header("Authorization", f"Bearer {jwt}")
+            latestLZMARequest.add_header(
+                "User-Agent", "liftinstall (j-selby)")
+            latestLZMAResponse = None
+            try:
+                latestLZMAResponse = urlopen(latestLZMARequest)
+            except urllib.error.HTTPError as httpError:
+                print(f"Error downloading LZMA ({httpError.code}) :(")
+                sys.exit(1)
 
-                latestFile = open(releaseEntry["name"], "wb")
-                latestFile.write(latestLZMAResponse.read())
-                latestFile.close()
+            latestFile = open(releaseEntry["name"], "wb")
+            latestFile.write(latestLZMAResponse.read())
+            latestFile.close()
 
-                print("Extracting...")
+            print("Extracting...")
 
-                with lzma.open(releaseEntry["name"]) as lzma:
-                    with tarfile.open(fileobj=lzma) as tar:
-                        if os.path.exists("./src"):
-                            shutil.rmtree("./src")
-                        content = tar.extractall("./src")
+            with lzma.open(releaseEntry["name"]) as lzma:
+                with tarfile.open(fileobj=lzma) as tar:
+                    if os.path.exists("./src"):
+                        shutil.rmtree("./src")
+                    tar.extractall("./src")
 
-                        os.remove(releaseEntry["name"])
+                    os.remove(releaseEntry["name"])
 
-                srcPath = f"./src/{releaseEntry['name'].replace('.tar.xz', '')}"
+            srcPath = f"./src/{releaseEntry['name'].replace('.tar.xz', '')}"
 
-                with open("build.sh", "w") as buildSH:
-                    # this filename would normally be sanitized but its already validated by the regex
-                    buildSH.write(
-                        f"cd {srcPath}\nmkdir build\ncd build\ncmake .. -GNinja\nninja"
-                    )
+            with open("build.sh", "w") as buildSH:
+                # this filename would normally be sanitized but its already validated by the regex
+                buildSH.write(
+                    f"cd {srcPath}\nmkdir build\ncd build\ncmake .. -GNinja\nninja"
+                )
 
-                print("Building...")
+            print("Building...")
 
-                startTime = datetime.datetime.now()
+            startTime = datetime.datetime.now()
 
-                with subprocess.Popen(
+            with subprocess.Popen(
                     ["bash", "build.sh"],
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE,
                     bufsize=1,
                     universal_newlines=True,
-                ) as process:
-                    for line in process.stdout:
-                        print(line, end="")  # process line here
-                    for line in process.stderr:
-                        print(line, file=sys.stderr, end="")
+            ) as process:
+                for line in process.stdout:
+                    print(line, end="")  # process line here
+                for line in process.stderr:
+                    print(line, file=sys.stderr, end="")
 
-                if process.returncode != 0:
-                    print(f"Failed build ({process.returncode}) :(")
-                    sys.exit(process.returncode)
-                buildTimeDelta = datetime.datetime.now()-startTime
-                # https://stackoverflow.com/a/14190143
-                hours = buildTimeDelta.seconds // 3600
-                minutes = (buildTimeDelta.seconds % 3600) // 60
-                seconds = (buildTimeDelta.seconds % 60)
-                print(
-                    f"Build successful! It took a solid {hours}H:{minutes}M:{seconds}S :)")
-                os.remove("build.sh")
-                print("Moving binaries...")
-                if not os.path.exists("./build"):
-                    os.mkdir("./build")
-                shutil.move(f"{srcPath}/build/bin/yuzu", "./build/yuzu")
-                shutil.move(f"{srcPath}/build/bin/yuzu-cmd",
-                            "./build/yuzu-cmd")
-                print("Removing source folder...")
-                shutil.rmtree("./src")
-                print("Writing version...")
-                f.truncate(0)
-                f.write(str(latestVersion))
-                print("Build finished, see the build folder for the binaries!")
+            if process.returncode != 0:
+                print(f"Failed build ({process.returncode}) :(")
+                sys.exit(process.returncode)
+            buildTimeDelta = datetime.datetime.now() - startTime
+            # https://stackoverflow.com/a/14190143
+            hours = buildTimeDelta.seconds // 3600
+            minutes = (buildTimeDelta.seconds % 3600) // 60
+            seconds = (buildTimeDelta.seconds % 60)
+            print(
+                f"Build successful! It took a solid {hours}H:{minutes}M:{seconds}S :)")
+            os.remove("build.sh")
+            print("Moving binaries...")
+            if not os.path.exists("./build"):
+                os.mkdir("./build")
+            shutil.move(f"{srcPath}/build/bin/yuzu", "./build/yuzu")
+            shutil.move(f"{srcPath}/build/bin/yuzu-cmd",
+                        "./build/yuzu-cmd")
+            print("Removing source folder...")
+            shutil.rmtree("./src")
+            print("Writing version...")
+            lastDownloadedVersionFile.close()
+            os.remove(LATEST_DOWNLOADED_VER_TXT_PATH)
+            lastDownloadedVersionFile = open(LATEST_DOWNLOADED_VER_TXT_PATH, "w")
+            lastDownloadedVersionFile.write(str(latestVersion))
+            lastDownloadedVersionFile.close()
+            print("Build finished, see the build folder for the binaries!")
